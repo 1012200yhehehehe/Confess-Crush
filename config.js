791125351832957bd2d439af08bf0dcef8b62512
1012200yhehehehe <94580261+1012200yhehehehe@users.noreply.github.com>
2021-11-18 09:14:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Demo",
-    introTitle: 'Nguyen van A',
+    introTitle: 'Cậu ưi',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Iu emkk nhìu lém <33',
+    btnNo: 'Khum, xàm vãi l :3',
+    question: 'Ultr kó ai mờ ngờ đôu thế giới có hưn 7 tị ngừi tsao anh lại iuk em <3',
+    btnReply: 'Gửi cho em đêm lun lin <3',
+    reply: 'Vì Thp xinh qué nên anh iuu bé đók  <33333333',
+    mess: 'Vâng 🥰 em biết Thp xinh mò 😘😘',
+    messDesc: 'nma anh yêu bék.',
+    btnAccept: 'Okiiiii lun iu ank <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
